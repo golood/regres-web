e2c1b1c4dce378fd347c70a1b7de3350b40fab8b
Andrey Shumilov <sumilovirk@gmail.com>
2020-02-10 22:09:01 +0800

    first commit. Add upload file.

---
+++ app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = '/home/golod/projects/regres-web/static'
+ALLOWED_EXTENSIONS = set(['txt'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template('main.html')
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+if __name__ == '__main__':
+    # Will make the server available externally as well
+    app.run()
---
+++ regres/regr.py
@@ -0,0 +1,156 @@
+import numpy as np
+from functools import reduce
+from pulp import LpVariable, LpMinimize, LpProblem
+import regres.rrrr as rrr
+import sys
+
+sys.setrecursionlimit(1000000000)
+
+x = np.array([[2., 5.],
+          [9., 4.],
+          [6., 1.],
+          [8., 3.],
+          [1., 7.],
+          [5., 8.]])
+
+y = np.array([7., 9., 1., 6., 4., 5.])
+#
+#
+h1 = [0, 1, 2]
+
+h2 = [3, 4, 5]
+#
+y1 = np.array([7, 9, 1])
+y2 = np.array([6, 4, 5])
+
+
+class Method:
+
+    def __init__(self, x, y):
+        self.y = np.array(y)
+        self.x = np.array(x)
+        self.a = []
+        self.eps = []
+        self.e = 0
+
+    def _y(self, alfa):
+        A = list(map(lambda item:
+                     list(map(lambda x, a: x * a, item,
+                              alfa)),
+                     self.x))
+        A = list(map(lambda item:
+                     reduce(lambda x, y: x + y, item), A))
+
+        return A
+
+    def epselon(self, alfa):
+        return list(
+            map(lambda x, y: y - x, self._y(alfa), self.y))
+
+    def Epselon(self, alfa):
+        mod = lambda x: x if (x > 0) else x * -1
+
+        E = 1 / len(self.y) * reduce(
+            lambda x, y: x + y,
+            list(map(lambda x, y: mod((y - x) / y),
+                     self._y(alfa), self.y))) * 100
+
+        return E
+
+    def getResaul(self):
+        return self.a, self.eps, self.e
+
+
+class MNK(Method):
+
+    def __init__(self, x, y):
+        super().__init__(x, y)
+
+    def find_a(self):
+        return np.dot(
+            np.dot(
+                np.linalg.inv(np.dot(self.x.T, self.x)),
+                self.x.T),
+            self.y)
+
+    def run(self):
+        a = self.find_a()
+        for item in a:
+            self.a.append(item[0])
+        eps = self.epselon(self.a)
+        for item in eps:
+            self.eps.append(item[0])
+        self.e = self.Epselon(self.a)[0]
+
+
+class MNM(Method):
+
+    def __init__(self, x, y):
+        super().__init__(x, y)
+
+    def find_a(self):
+        pass
+
+    def run(self):
+        task = rrr.LpSolve_MNM(self.x, self.y)
+        task.run()
+        self.a, self.eps = task.getResault()
+        self.e = self.Epselon(self.a)[0]
+
+
+class MAO(Method):
+
+    def __init__(self, x, y):
+        super().__init__(x, y)
+
+    def find_a(self):
+        pass
+
+    def run(self):
+        task = rrr.LpSolve_MAO(self.x, self.y)
+        task.run()
+        self.a, self.eps = task.getResault()
+        self.e = self.Epselon(self.a)[0]
+
+class MCO(Method):
+
+    def __init__(self, x, y, h1, h2):
+        super().__init__(x, y)
+        self.h1 = h1
+        self.h2 = h2
+
+    def find_a(self):
+        pass
+
+    def run(self):
+        task = rrr.LpSolve_MCO(self.x, self.y, self.h1, self.h2)
+        task.run()
+        self.a, self.eps = task.getResault()
+        self.e = self.Epselon(self.a)[0]
+
+
+class Task:
+
+    def __init__(self, x, y, h1=None, h2=None):
+        self.methods = []
+        self.methods.append(MNK(x, y))
+        self.methods.append(MNM(x, y))
+        self.methods.append(MAO(x, y))
+        self.methods.append(MCO(x, y, h1, h2))
+
+    def run(self):
+
+        for item in self.methods:
+            item.run()
+
+    def getResaults(self):
+        resaults = []
+
+        for item in self.methods:
+            resaults.append(item.getResaul())
+
+        return resaults
+
+# test = Task(x, y, h1, h2)
+# test.run()
+# test.getResaults()
