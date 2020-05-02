79494686edfdb1a93a7b2f5c88637314262c1041
Andrey Shumilov <sumilovirk@gmail.com>
2020-05-02 23:29:13 +0800

    Добавил временные изменения для тестирования. Вернул очередь.

--- regres/regr.py
+++ regres/regr.py
@@ -243,21 +243,20 @@ class TaskPerebor:
         '''
 
         startTime = datetime.datetime.now()
-#        queue = Queue()
+        queue = Queue()
 
         # Запускаем поток и очередь
-#        for i in range(2):
- #           t = self.MyThread(queue)
-#            t.setDaemon(True)
-#            t.start()
+        for i in range(2):
+            t = self.MyThread(queue)
+            t.setDaemon(True)
+            t.start()
 
         # Даем очереди нужные нам задачи для решения
         for task in self.tasks:
-#            queue.put(task)
-            task.run()
+            queue.put(task)
 
         # Ждем завершения работы очереди
-#        queue.join()
+        queue.join()
 
         endTime = datetime.datetime.now()
         s = str((endTime - startTime).total_seconds())
