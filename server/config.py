07064f305d5ea522e8a9e8a1717f1b36e3cf8c8e
Andrey Shumilov <sumilovirk@gmail.com>
2020-09-14 00:12:00 +0800

    Поправил конфиг.

--- server/config.py
+++ server/config.py
@@ -4,6 +4,7 @@ postgres_db = os.environ.get('POSTGRES_DB')
 postgres_user = os.environ.get('POSTGRES_USER')
 postgres_password = os.environ.get('POSTGRES_PASSWORD')
 postgres_host = os.environ.get('POSTGRES_HOST')
+postgres_port = '5432'
 
 secret_key = os.environ.get('SECRET_KEY')
 
